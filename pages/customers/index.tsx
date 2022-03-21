b345511c650145c102a2715dc15c3e199c4e66a7
Matheus <mf5881585@gmail.com>
2022-03-21 18:15:48 -0300

    added a button to new customers page

--- pages/customers/index.tsx
+++ pages/customers/index.tsx
@@ -1,3 +1,17 @@
-const Customers = () => "Customers";
+import type { NextPage } from "next";
+import { Button, Container } from "@chakra-ui/react";
+import Link from "next/link";
+
+const Customers: NextPage = () => {
+  return (
+    <Container>
+      <Link href="/customers/new">
+        <Button colorScheme="blue" marginTop="10px">
+          Novo cliente
+        </Button>
+      </Link>
+    </Container>
+  );
+};
 
 export default Customers;
