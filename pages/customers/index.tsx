5fc6af28cf40da690a01e9b6c9a396999c1404ca
Matheus <mf5881585@gmail.com>
2022-03-22 14:30:03 -0300

    added a customers' table on customers page

--- pages/customers/index.tsx
+++ pages/customers/index.tsx
@@ -1,15 +1,39 @@
 import type { NextPage } from "next";
-import { Button, Container } from "@chakra-ui/react";
+import { Button, Container, Heading } from "@chakra-ui/react";
+import { Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
 import Link from "next/link";
 
 const Customers: NextPage = () => {
   return (
     <Container>
+      <Heading>Clientes</Heading>
       <Link href="/customers/new">
-        <Button colorScheme="blue" marginTop="10px">
+        <Button colorScheme="blue" marginBottom="10px" float="right">
           Novo cliente
         </Button>
       </Link>
+      <Table variant="simple">
+        <Thead>
+          <Tr>
+            <Th>Nome</Th>
+            <Th>Telefone</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Td>José Manoel</Td>
+            <Td>(000) 000-0000</Td>
+          </Tr>
+          <Tr>
+            <Td></Td>
+            <Td></Td>
+          </Tr>
+          <Tr>
+            <Td></Td>
+            <Td></Td>
+          </Tr>
+        </Tbody>
+      </Table>
     </Container>
   );
 };
