d064acf2263c98ef631bdf1b63354933daef5ea6
matheusqueiroz1 <100311328+matheusqueiroz1@users.noreply.github.com>
2022-03-22 15:22:22 -0300

    feat: goto customer page button at dashboard and
    * added a dashboard button to customers page
    
    * added a button to new customers page
    
    * added a customers' table on customers page
    
    Co-authored-by: Matheus <mf5881585@gmail.com>

--- pages/customers/index.tsx
+++ pages/customers/index.tsx
@@ -1,3 +1,41 @@
-const Customers = () => "Customers";
+import type { NextPage } from "next";
+import { Button, Container, Heading } from "@chakra-ui/react";
+import { Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
+import Link from "next/link";
+
+const Customers: NextPage = () => {
+  return (
+    <Container>
+      <Heading>Clientes</Heading>
+      <Link href="/customers/new">
+        <Button colorScheme="blue" marginBottom="10px" float="right">
+          Novo cliente
+        </Button>
+      </Link>
+      <Table variant="simple">
+        <Thead>
+          <Tr>
+            <Th>Nome</Th>
+            <Th>Telefone</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Td>José Manoel</Td>
+            <Td>(000) 000-0000</Td>
+          </Tr>
+          <Tr>
+            <Td></Td>
+            <Td></Td>
+          </Tr>
+          <Tr>
+            <Td></Td>
+            <Td></Td>
+          </Tr>
+        </Tbody>
+      </Table>
+    </Container>
+  );
+};
 
 export default Customers;
