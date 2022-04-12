8e852af8d929ac3792293bbc675782438fc16e3b
Cleiton P. Queiroz <cleitonpqz@gmail.com>
2022-04-12 15:57:37 -0300

    feat: created invoice page and minor adjustments to layout

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,4 +1,18 @@
-interface Customer {
+interface Base {
+  _id?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+interface Customer extends Base {
   name: string;
   phoneNumber: string;
+}
+
+interface Invoice extends Base {
+  customer_id: string;
+  item: string;
+  totalAmount: number;
+  dueDate: Date;
+  paymentFrequency: string;
+  numberOfPayments: number;
 }
--- components/Layout.js
+++ components/Layout.js
@@ -1,19 +1,23 @@
 import Head from "next/head";
-import { Box } from '@chakra-ui/react'
-import NavBar from "./NavBar.tsx"
-import Footer from "./Footer.tsx"
+import { Box, Center } from "@chakra-ui/react";
+import NavBar from "./NavBar.tsx";
+import Footer from "./Footer.tsx";
 
 export default function Layout({ children }) {
-    return (
-        <>
-            <Head>
-                <title>Machado Multiservices Contract Managment</title>
-                <meta name="description" content="Contract management" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <NavBar/>
-            <Box minH={570}>{ children }</Box>
-            <Footer/>
-        </>
-    )
+  return (
+    <>
+      <Head>
+        <title>Machado Multiservices Contract Managment</title>
+        <meta name="description" content="Contract management" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Center>
+        <Box width="6xl">
+          <NavBar />
+          <Box minH={570}>{children}</Box>
+          <Footer />
+        </Box>
+      </Center>
+    </>
+  );
 }
